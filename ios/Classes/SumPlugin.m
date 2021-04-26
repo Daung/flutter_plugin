72070ae63092d215e43e4ee7b07b8f11c3e879fb
wxc <wxc@yiban.cn>
2021-04-26 10:58:43 +0800

    添加ios 端实现逻辑

--- ios/Classes/SumPlugin.m
+++ ios/Classes/SumPlugin.m
@@ -12,7 +12,11 @@
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"getPlatformVersion" isEqualToString:call.method]) {
     result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
+  }else if ([@"sum" isEqualToString:call.method]) {
+      NSInteger a = [call.arguments[@"num1"] intValue];
+      NSInteger b = [call.arguments[@"num2"] intValue];
+      result([NSNumber numberWithInt:(a + b)]);
+    } else {
     result(FlutterMethodNotImplemented);
   }
 }
